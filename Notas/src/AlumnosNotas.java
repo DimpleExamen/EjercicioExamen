d7c34c892c7453a198b20bd31d2b487470d2bae3
dimpl <dimpl@LAPTOP-V0PHOVTT>
2019-05-26 08:42:20 +0100

    Versión 1:
    Proyecto que describe las notas de los almunos introducidos.

---
+++ Notas/src/AlumnosNotas.java
@@ -0,0 +1,23 @@
+public class AlumnosNotas {
+public static void main(String[] args) {
+
+	  String[] Nombre = {"Carlota", "Carlos", "Diego", "Laura"};
+      int[] Nota = {0, 5, 8, 10};
+      String[] Calificacion = new String[4];
+      
+     for (int i=0; i<Nota.length; i++){
+               
+        do{
+            if(Nota[i] < 5){Calificacion[i] = "Suspenso";}
+            else if(Nota[i] >= 5 && Nota[i] < 7) {Calificacion[i] = "Aprobado";}
+            else if(Nota[i] >= 7 && Nota[i] < 9) {Calificacion[i] = "Notable";}
+            else if(Nota[i] >= 9 && Nota[i] <= 10) {Calificacion[i] = "Sobresaliente";}
+            else {System.out.println("ERROR EN LA NOTA") ;}
+            
+        } 
+        while(Nota[i] < 0 || Nota[i] > 10);
+        System.out.println(Nombre[i] +  ": " + Calificacion[i]);
+        
+       }    
+	}
+ }
